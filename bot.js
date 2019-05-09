1186b52ad97bd1f532a5ec3baeaf5b9dd34ddeb6
PeterFareed <46231250+PeterFareed@users.noreply.github.com>
2019-05-09 15:46:49 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("573926001353162763")
+setInterval(function() {
+channel.send(`ii7aMoDY_xDii7aMoDY_xDii7aMoDY_xDii7aMoDY_xDii7aMoDY_xDii7aMoDY_xDii7aMoDY_xDii7aMoDY_xDii7aMoDY_xDii7aMoDY_xD`)
+
+client.login(process.env.BOT_TOKEN);
